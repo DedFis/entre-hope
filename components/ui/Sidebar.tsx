dc6ecf2f2d2ba92a82f2e0898c6709680bedb532
DeadFish <medygunawan32@gmail.com>
2024-11-20 16:03:22 +0700

    Sidebar Active Page Fix

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -31,12 +31,12 @@ const Icon: React.FC<IconProps> = ({ styles, name, imgUrl, isActive, disabled, h
 
 const Sidebar: React.FC = () => {
   const router = useRouter();
-  const [isActive, setIsActive] = useState('dashboard');
+  const [isActive, setIsActive] = useState('homepage');
 
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
       <Link href="/">
-          <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} name="logo" isActive={isActive} handleClick={() => {}} />
+          <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} name="logo" isActive={isActive} handleClick={() => {setIsActive('homepage')}} />
       </Link>
 
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
